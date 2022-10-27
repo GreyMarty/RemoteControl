a7edfc73df45d40592063a8c344d92ab37c11656
GreyMarty <56483433+GreyMarty@users.noreply.github.com>
2022-10-27 21:33:42 +0300

    Update Program.cs

--- RemoteControl.Server.UI/Program.cs
+++ RemoteControl.Server.UI/Program.cs
@@ -1,11 +1,1 @@
-﻿using System.Threading;
-using RemoteControl.Core;
-using RemoteControl.IO;
 
-
-for (int i = 0; i < 100; i++) 
-{
-    //Mouse.Move(new PointI(1, 1));
-    Mouse.Scroll(-1000);
-    Thread.Sleep(1);
-}
